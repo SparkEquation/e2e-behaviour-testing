e993e299b5f251a3aa886511c17f0bfedfd19ee0
Daniil Sloboda <daniil.sloboda@sparkequation.com>
2019-08-15 17:53:49 +0300

    Trying to create pageObjects dir and gitignore file

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-console.log(path.resolve('..', '..'));
-console.log(__dirname);
-console.log(path.resolve(__dirname));
+const integratedRepoBasePath = path.resolve(__dirname, '..', '..');
+const cypressPageObjectsPath = path.resolve(integratedRepoBasePath, 'cypress', 'integration', 'pageObjects');
+const gitignorePath = path.resolve(cypressPageObjectsPath, '.gitignore');
+const gitignoreContent = `index.ts`;
+
+fs.mkdirSync(path.resolve(cypressPageObjectsPath), { recursive: true });
+
+if(!fs.existsSync(gitignorePath)) {
+    fs.writeFileSync(gitignorePath, gitignoreContent);
+}
