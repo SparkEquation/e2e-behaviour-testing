4fe46de42f3c58e29988f237a852d32503d52a4f
Daniil Sloboda <daniil.sloboda@sparkequation.com>
2019-08-19 15:13:39 +0300

    Added parsing of nested page objects structure

--- pageObjectImportsGenerator.js
+++ pageObjectImportsGenerator.js
@@ -2,25 +2,47 @@
 
 const fs = require('fs');
 const path = require('path');
+const posixPath = path.posix;
 
 // TODO add chokidar to automatically rearrange imports on change https://github.com/paulmillr/chokidar
 
 const INDEX_FILENAME = 'index.ts';
-
-// Filter only typescript and javascript page object files and not index file
-const filesFilter = filename => filename.match(/\.(ts|js)$/) && filename !== INDEX_FILENAME;
+// Max depth of page objects files
+const MAX_DEPTH = 10;
+const AVAILABLE_EXTENSIONS = ['.js', '.ts'];
 
 //Remove extensions and encapsulate single quotes
 const filesMap = filename => filename.slice(0, -3).replace(`'`, `\\'`);
 
 // Create file with imports
-const filesReduce = (acc, filename) => acc + `import './${filename}';\n`;
+const filesReduce = (acc, filename) => acc + `import '${filename}';\n`;
+
+function getFiles (pageObjectsPath, nestedPath = '.') {
+    const files = fs.readdirSync(
+        path.resolve(pageObjectsPath, nestedPath),
+        { withFileTypes: true }
+    );
+
+    return files.map(dirent => {
+            if (dirent.isDirectory()) {
+                return getFiles(pageObjectsPath, path.join(nestedPath, dirent.name));
+            }
+            return path.join(nestedPath, dirent.name);
+        })
+        .flat(MAX_DEPTH);
+}
 
 const pageObjectDirPath = path.resolve('cypress', 'integration', 'pageObjects');
 
-const fileNames = fs.readdirSync(pageObjectDirPath)
-    .filter(filesFilter)
+const files = getFiles(pageObjectDirPath)
+    .filter(
+        filePath => AVAILABLE_EXTENSIONS.includes(path.extname(filePath)) && filePath !== INDEX_FILENAME
+    )
     .map(filesMap)
-    .reduce(filesReduce, '');
+    .map(file => {
+        return `./${posixPath.join(...file.split(path.sep))}`;
+    });
 
-fs.writeFileSync(path.resolve(pageObjectDirPath, 'index.ts'), fileNames);
+const importContent = files.reduce(filesReduce, '');
+
+fs.writeFileSync(path.resolve(pageObjectDirPath, 'index.ts'), importContent);
